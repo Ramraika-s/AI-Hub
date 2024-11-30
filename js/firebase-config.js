b9e53d26b4ab5fff703d9f7834c71c75b07be32d
Ramraika-s <shivamramraika01@gmail.com>
2024-11-30 07:17:40 +0530

    up

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: 'your_firebase_api_key', // Use your Firebase API key
-  authDomain: 'your_firebase_project_id.firebaseapp.com',
-  projectId: 'your_firebase_project_id',
-  storageBucket: 'your_firebase_project_id.appspot.com',
-  messagingSenderId: 'your_firebase_sender_id',
-  appId: 'your_firebase_app_id',
-  measurementId: 'your_firebase_measurement_id'
+  apiKey: 'AIzaSyCl7PnoVlHFWdPJqLDdIYxXCXGAk8plDxI', // Use your Firebase API key
+  authDomain: 'aihub-services.firebaseapp.com',
+  projectId: 'aihub-services',
+  storageBucket: 'aihub-services.appspot.com',
+  messagingSenderId: '544621705118',
+  appId: "1:544621705118:web:9a3d02ed2a2c632a37bc68",
+  measurementId: 'G-JPZTWM3MTP'
 };
 
 // Initialize Firebase
